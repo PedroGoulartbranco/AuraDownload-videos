89dbd08f8518f5676de1b39ff2125913fa34d341
Vitor Rovani Marcelino <vitinho.marcelino2017@gmail.com>
2026-03-28 19:51:48 -0300

    up

--- frontend/.next/dev/static/chunks/src_app_page_tsx_002ye8~._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_11ogzf9._.js","static/chunks/src_0j~dc_3._.js"],
-    source: "dynamic"
-});

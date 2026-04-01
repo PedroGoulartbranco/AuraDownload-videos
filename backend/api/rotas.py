ae25ec2954da64f4f56bad789ef8a80dfb66c125
Pedro Goulart Branco <pgoulartbranco@gmail.com>
2026-04-01 15:36:07 -0300

    implement global input sanitization and link validation across all routes

--- backend/api/rotas.py
+++ backend/api/rotas.py
@@ -17,7 +17,6 @@ download_limite = asyncio.Semaphore(50) #Limita o servidor para ter no maximo 50
 async def Youtube_mandar_informacoes_video(requisicao: VideoRequest):
     link_recebido = requisicao.url
     link_seguro, link_recebido = verificar_link_youtube(link_recebido)
-    print("------------", link_seguro)
     if link_seguro:
         informacoes_video = youtube_informacoes_video(link_recebido)
         return {
@@ -38,18 +37,31 @@ async def youtube_baixar_video(qualidade: VideoQuality, background_tasks: Backgr
     async with download_limite:
         link_recebido = qualidade.url
         qualidade_video = qualidade.resolution
-        caminho_video = youtube_baixar_videos(link_recebido, qualidade_video)
-        if caminho_video and os.path.exists(caminho_video):
-            print(os.path.basename(caminho_video))
-            background_tasks.add_task(excluir_video,caminho_video) #Não pode colocar () na função porque se nao executa na hora
-            return FileResponse(
-                path=caminho_video, 
-                filename=os.path.basename(caminho_video), # Pega só o nome do arquivo
-                media_type='video/mp4'
-            )
-        raise HTTPException(
+        link_seguro, link_recebido = verificar_link_youtube(link_recebido)
+        if link_seguro:
+            try:
+                caminho_video = youtube_baixar_videos(link_recebido, qualidade_video)
+                if caminho_video and os.path.exists(caminho_video):
+                    print(os.path.basename(caminho_video))
+                    background_tasks.add_task(excluir_video,caminho_video) #Não pode colocar () na função porque se nao executa na hora
+                    return FileResponse(
+                        path=caminho_video, 
+                        filename=os.path.basename(caminho_video), # Pega só o nome do arquivo
+                        media_type='video/mp4'
+                    )
+                raise HTTPException( #caso de erro mesmo com a função dando certa
+                    status_code=400, 
+                    detail="Falha no dowload do arquivo"
+                )
+            except: #Caso de erro na função em geral por causa do link
+                raise HTTPException(
+                    status_code=400, 
+                    detail="Não foi possível baixar o vídeo. Verifique se o link do YouTube é válido ou se a qualidade escolhida está disponível."
+                )
+        else: #Caso não seja seguro
+            raise HTTPException(
             status_code=400, 
-            detail="Não foi possível baixar o vídeo. Verifique se o link do YouTube é válido ou se a qualidade escolhida está disponível."
+            detail="Link inválido, malicioso ou playlist não suportada."
         )
     
 @router.post("/baixar_audio_youtube")
@@ -60,29 +72,67 @@ async def youtube_baixar_video(background_tasks: BackgroundTasks, request: Reque
     async with download_limite:
         link_recebido = qualidade.url
         qualidade_audio = qualidade.quality_audio
-        caminho_audio = youtube_baixar_audio(link_recebido, qualidade_audio)
-        if caminho_audio and os.path.exists(caminho_audio):
-            background_tasks.add_task(excluir_video,caminho_audio) #Não pode colocar () na função porque se nao executa na hora
-            return FileResponse(
-                path=caminho_audio, 
-                filename=os.path.basename(caminho_audio), # Pega só o nome do arquivo
-                media_type='audio/mpeg'
-            )
-        raise HTTPException(
+        link_seguro, link_recebido = verificar_link_youtube(link_recebido)
+        if link_seguro:
+            try:
+                caminho_audio = youtube_baixar_audio(link_recebido, qualidade_audio)
+                if caminho_audio and os.path.exists(caminho_audio):
+                    background_tasks.add_task(excluir_video,caminho_audio) #Não pode colocar () na função porque se nao executa na hora
+                    return FileResponse(
+                        path=caminho_audio, 
+                        filename=os.path.basename(caminho_audio), # Pega só o nome do arquivo
+                        media_type='audio/mpeg'
+                    )
+                raise HTTPException(
+                    status_code=400, 
+                    detail="Não foi possível baixar o áudio. Verifique se o link do YouTube é válido ou se a qualidade escolhida está disponível."
+                )
+            except:
+                raise HTTPException(
+                    status_code=400, 
+                    detail="Não foi possível baixar o áudio. Verifique se o link do YouTube é válido ou se a qualidade escolhida está disponível."
+                )
+        else: #Caso não seja seguro o link
+            raise HTTPException(
             status_code=400, 
-            detail="Não foi possível baixar o áudio. Verifique se o link do YouTube é válido ou se a qualidade escolhida está disponível."
+            detail="Link inválido, malicioso ou playlist não suportada."
         )
 
 @router.post("/youtube_tamanho_qualidade")
 async def youtube_mandar_tamanho(requisicao: VideoRequest):
      link_recebido = requisicao.url
-     tamanhos = mandar_tamanho_resolucoes(link_recebido)
-     return  tamanhos
+     link_seguro, link_recebido = verificar_link_youtube(link_recebido)
+     if link_seguro:
+        try:
+            tamanhos = mandar_tamanho_resolucoes(link_recebido)
+            return  tamanhos
+        except:
+            raise HTTPException(
+            status_code=400, 
+            detail="Erro nos tamanhos do arquivo"
+        )
+     else:
+        raise HTTPException(
+            status_code=400, 
+            detail="Link inválido, malicioso ou playlist não suportada."
+        )
 
 @router.post("/youtube_audio_tamanho")
 async def youtube_audio_mandar_tamanho(requisicao: VideoRequest):
-     link_recebido = requisicao.url
-     tamanhos = youtube_mandar_audio_tamanho(link_recebido)
-     return tamanhos      
-
+    link_recebido = requisicao.url
+    link_seguro, link_recebido = verificar_link_youtube(link_recebido)
+    if link_seguro:
+        try:
+            tamanhos = youtube_mandar_audio_tamanho(link_recebido)
+            return tamanhos
+        except:
+            raise HTTPException(
+            status_code=400, 
+            detail="Erro nos tamanhos do arquivo"
+            ) 
+    else:
+        raise HTTPException(
+            status_code=400, 
+            detail="Link inválido, malicioso ou playlist não suportada."
+        )
 #@router.pos("")
